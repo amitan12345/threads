4d81a2d9e5e546da584cce4ac871afaa23f67e31
amitan12345 <03_habitat.shelter@icloud.com>
2023-10-08 12:13:25 +0900

    chore: :hammer: Add tailwind and flowbite scripts in livewire base blade

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     </head>
     <body>
         {{ $slot }}
