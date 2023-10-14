3122025d2b09d1e19ab3885b8fc8e718b97584d1
amitan12345 <03_habitat.shelter@icloud.com>
2023-10-14 19:00:09 +0900

    chore: :lipstick: Change tweet list background color

--- resources/views/livewire/user-tweet-list.blade.php
+++ resources/views/livewire/user-tweet-list.blade.php
@@ -1,6 +1,6 @@
 <div class="overflow-auto h-full">
     @foreach($tweets as $tweet)
-        <div class="h-32 bg-yellow-400 border-b-4 border-indigo-400">
+        <div class="h-32 bg-blue-200 border-b-4 border-indigo-600">
             <div>{{ $tweet->text }}</div>
             <div>{{ $tweet->created_at }}</div>
         </div>
