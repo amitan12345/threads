16e0877403f1f7a635644f4d03e5e030358374be
amitan12345 <03_habitat.shelter@icloud.com>
2023-10-15 10:39:56 +0900

    feat: :sparkles: Made thread list column can click

--- resources/views/livewire/user-thread-list.blade.php
+++ resources/views/livewire/user-thread-list.blade.php
@@ -1,9 +1,11 @@
 <div class="overflow-auto h-full">
     @foreach($threads as $thread)
-        <div class="h-32 bg-yellow-300 border-b-4 border-indigo-600">
-            <div>{{ $thread->name }}</div>
-            <div>{{ $thread->description }}</div>
-            <div>{{ $thread->created_at }}</div>
-        </div>
+        <a href="/thread/{{ $thread->id }}">
+            <div class="h-32 bg-yellow-300 border-b-4 border-indigo-600 hover:bg-yellow-600">
+                <div>{{ $thread->name }}</div>
+                <div>{{ $thread->description }}</div>
+                <div>{{ $thread->created_at }}</div>
+            </div>
+        </a>
     @endforeach
 </div>
