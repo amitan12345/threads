94726c453b15327164088e4aaa7698fc668dcaa1
amitan12345 <03_habitat.shelter@icloud.com>
2023-10-21 14:45:10 +0900

    chore: :package: Made tailwind using npm

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite('resources/css/app.css')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     </head>
     <body>
